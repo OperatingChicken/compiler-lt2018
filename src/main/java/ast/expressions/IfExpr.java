OperatingChicken/compiler-lt2018

commit 603147e37969d0618fe1d9d79feec225ed307a2a

Cleanup various unused variables and imports

src/main/java/ast/expressions/IfExpr.java
@@ -28,7 +28,6 @@ public class IfExpr extends Expr {
 
     @Override
     public LLVM.LLVMValueRef codeGen(CodeGen codegen) {
-        LLVMBasicBlockRef currentBB = codegen.popBlockStack();
         LLVMBasicBlockRef condBB = codegen.newBlock();
         LLVMBasicBlockRef ifTrueBB = codegen.newBlock();
         LLVMBasicBlockRef ifFalseBB = codegen.newBlock();

src/main/java/ast/statements/LoopStmt.java
@@ -19,7 +19,6 @@ public class LoopStmt extends Stmt {
     }
 
     public void codeGen(CodeGen codegen) {
-        LLVMBasicBlockRef currentBB = codegen.popBlockStack();
         LLVMBasicBlockRef condBB = codegen.newBlock();
         LLVMBasicBlockRef bodyBB = codegen.newBlock();
         LLVMBasicBlockRef afterBB = codegen.newBlock();
@@ -27,12 +26,10 @@ public class LoopStmt extends Stmt {
         LLVMPositionBuilderAtEnd(codegen.getBuilder(), condBB);
         codegen.pushBlockStack(condBB);
         LLVMValueRef cond_value = codegen.getBoolean(this.condition.codeGen(codegen));
-        LLVMBasicBlockRef newCondBB = codegen.popBlockStack();
         LLVMBuildCondBr(codegen.getBuilder(), cond_value, bodyBB, afterBB);
         LLVMPositionBuilderAtEnd(codegen.getBuilder(), bodyBB);
         codegen.pushBlockStack(bodyBB);
         this.body.codeGen(codegen);
-        LLVMBasicBlockRef newBodyBB = codegen.popBlockStack();
         LLVMBuildBr(codegen.getBuilder(), condBB);
         LLVMPositionBuilderAtEnd(codegen.getBuilder(), afterBB);
         codegen.pushBlockStack(afterBB);

src/main/java/cg/CodeGen.java
@@ -117,7 +117,6 @@ public class CodeGen {
     }
 
     public void emitObj(String path, boolean debug) {
-        LLVMBasicBlockRef main_block = this.blocks.pop();
         if(!this.blocks.empty()) {
             System.err.println("Internal stack error!");
             System.exit(1);

src/main/java/App.java
@@ -1,5 +1,4 @@
 import ast.statements.Stmt;
-import err.AbstractError;
 import java_cup.runtime.ComplexSymbolFactory;
 import cg.CodeGen;
 

commit 4fbdbb4e2fdf7ebb1a77914630eaa30c3cd073d4

Fix breaking changes introduced in 603147e

src/main/java/ast/expressions/IfExpr.java
@@ -28,6 +28,7 @@ public class IfExpr extends Expr {
 
     @Override
     public LLVM.LLVMValueRef codeGen(CodeGen codegen) {
+        codegen.popBlockStack();
         LLVMBasicBlockRef condBB = codegen.newBlock();
         LLVMBasicBlockRef ifTrueBB = codegen.newBlock();
         LLVMBasicBlockRef ifFalseBB = codegen.newBlock();

src/main/java/ast/statements/LoopStmt.java
@@ -21,6 +21,7 @@ public class LoopStmt extends Stmt {
 
     @Override
     public void codeGen(CodeGen codegen) {
+        codegen.popBlockStack();
         LLVMBasicBlockRef condBB = codegen.newBlock();
         LLVMBasicBlockRef bodyBB = codegen.newBlock();
         LLVMBasicBlockRef afterBB = codegen.newBlock();
@@ -28,10 +29,12 @@ public class LoopStmt extends Stmt {
         LLVMPositionBuilderAtEnd(codegen.getBuilder(), condBB);
         codegen.pushBlockStack(condBB);
         LLVMValueRef cond_value = codegen.getBoolean(this.condition.codeGen(codegen));
+        codegen.popBlockStack();
         LLVMBuildCondBr(codegen.getBuilder(), cond_value, bodyBB, afterBB);
         LLVMPositionBuilderAtEnd(codegen.getBuilder(), bodyBB);
         codegen.pushBlockStack(bodyBB);
         this.body.codeGen(codegen);
+        codegen.popBlockStack();
         LLVMBuildBr(codegen.getBuilder(), condBB);
         LLVMPositionBuilderAtEnd(codegen.getBuilder(), afterBB);
         codegen.pushBlockStack(afterBB);

src/main/java/cg/CodeGen.java
@@ -117,6 +117,7 @@ public class CodeGen {
     }
 
     public void emitObj(String path, boolean debug) {
+        this.blocks.pop();
         if(!this.blocks.empty()) {
             System.err.println("Internal stack error!");
             System.exit(1);