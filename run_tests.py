a18cb7c7937882625f23d851cebf6408522aee01
Gianluca Nitti <gianluca.nitti96@gmail.com>
2018-06-10 19:22:39 +0200

    Properly return nonzero exit code when tests fail

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,7 @@ import json
 import subprocess
 import platform
 import tempfile
+import sys
 
 if __name__ == "__main__":
     if platform.system() == "Windows":
@@ -11,6 +12,7 @@ if __name__ == "__main__":
     else:
         subprocess.run(["./gradlew", "jar"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     config = json.loads(open("tests/config.json", "r").read())
+    successfulTests = 0
     for test in config:
         print(test["name"], ": ", end='', sep='')
         with tempfile.TemporaryDirectory() as tempdir:
@@ -20,11 +22,17 @@ if __name__ == "__main__":
                 continue
             if test["compileShouldFail"]:
                 print("OK")
+                successfulTests += 1
                 continue
             status = subprocess.run([tempdir + "/output.bin"], input=test["input"].encode(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             if status.returncode != 0:
                 print("FAIL (non-zero return code)")
             if test["output"] == status.stdout.decode():
                 print("OK")
+                successfulTests += 1
             else:
                 print("FAIL (different output)")
+    totalTests = len(config)
+    failedTests = totalTests - successfulTests
+    print("%d test(s) ran - %d passed, %d failed" % (totalTests, successfulTests, failedTests))
+    sys.exit(0 if successfulTests == totalTests else 1)
